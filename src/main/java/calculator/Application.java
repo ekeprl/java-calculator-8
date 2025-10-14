66c02af977366bc00d4bedab00fa8b3ec8766e00
KUN <yoonkun1902@gmail.com>
2025-10-14 17:57:41 +0900

    git add, commit 테스트

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -2,6 +2,12 @@ package calculator;
 
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+
+        calculate();
     }
+
+    private static void calculate() {
+
+    }
+
 }
